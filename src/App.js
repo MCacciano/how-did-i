e9542dac9eba9c79e1634c80312ca7ab4b264032
mcacciano <michael.cacciano@gmail.com>
2021-06-26 09:01:25 -0400

    login with google, can fetch from firestore

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { useEffect } from 'react';
+import { auth, db, signInWithGoogle } from './firebase';
+
 const App = () => {
+  useEffect(() => {
+    const unsub = auth.onAuthStateChanged((user) => {
+      console.log(`user`, user);
+      return user;
+    });
+
+    const getUsers = async () => {
+      const usersRef = db.collection('users');
+      const snapshot = await usersRef.get();
+
+      snapshot.forEach((doc) => {
+        console.log('doc', doc.exists);
+      });
+    };
+
+    getUsers();
+
+    return () => unsub();
+  }, [auth, db]);
+
   return (
     <div>
       <h1 className='text-3xl text-blue-800'>App</h1>
+      <button type='button' onClick={signInWithGoogle}>
+        Sign In With Google
+      </button>
     </div>
   );
 };
